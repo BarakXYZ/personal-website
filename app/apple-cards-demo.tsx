8c386235884fd6637bdd403a60c6f8127494fb9d
BarakXYZ <iBarakXYZ@gmail.com>
2025-04-11 13:35:02 +0300

    Configure tab title and favicon

--- app/apple-cards-demo.tsx
+++ app/apple-cards-demo.tsx
@@ -22,7 +22,7 @@ export function AppleCardsCarouselDemo() {
   return (
     <div className="w-full h-full py-20">
       <h2 className="max-w-7xl pl-4 mx-auto text-xl md:text-5xl font-bold text-neutral-800 dark:text-neutral-200 font-sans">
-        Videos I made.
+        Videos I made:
       </h2>
       <Carousel items={cards} />
     </div>
